1b9f5ec354f79205ccbc0b72ea9e1a405dddd2af
Souzzadev <souzzadev@gmail.com>
2026-08-16 23:31:02 -0300

    first commit

---
+++ perfil.py
@@ -0,0 +1,15 @@
+print("="*66)
+print(f"{"MEU PERFIL EM PYTHON":^66}")
+print("="*66 + "\n")
+print("Nome: Mariana")
+print("Área: Análise e Desenvolvimento de sistemas") 
+print("Linguagem atual: Python") 
+print("Objetivo: Tornar-me uma desenvolvedora Python \n") 
+print("Atualmente estudando:")
+print("- Python")
+print("- Lógica de programação \n")
+print("Este é meu primeiro projeto em Python! \n")
+print("="*66)
+
+
+
